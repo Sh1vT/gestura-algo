61811ce82c158270aa5c1af6b2594963d72b2ac0
ppkekw <accplaceholder311@gmail.com>
2025-04-27 21:58:59 +0530

    web-front added

---
+++ others/web-front/src/main/java/com/predictor/predictor/controller/APIControllers.java
@@ -0,0 +1,13 @@
+package com.predictor.predictor.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class APIControllers {
+    @GetMapping("/predictor")
+    public String predictor(){
+        System.out.println("Predictor");
+        return "predictor";
+    }
+}
